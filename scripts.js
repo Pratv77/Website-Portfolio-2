cc47143dd08f5fe8b9aa688f538e62c009fe0c7a
Prat <pratv7@outlook.com>
2024-07-26 14:49:58 -0400

    added card transition

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+
+// Custom Cursor
+
 const cursorDot = document.querySelector("[data-cursor-dot]");
 const cursorOutline = document.querySelector("[data-cursor-outline");
 
@@ -14,13 +17,19 @@ window.addEventListener("mousemove", function (e) {
     }, {duration: 500, fill: "forwards"});
 });
 
-document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
-        const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-        
-        card.style.setProperty("--mouse-x", `${x}px`);
-        card.style.setProperty("--mouse-y", `${y}px`);
-    }
+// Card Hover Effect
+
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+for(const card of document.querySelectorAll(".card")) {
+    card.onmousemove = e => handleOnMouseMove(e);
 }
